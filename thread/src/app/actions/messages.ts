16a351e5137f166bef9da9a05fb1684c6cb252ec
mindsek <contactmindsek@gmail.com>
2025-05-14 10:06:11 +0200

    refactor: add zod for config

--- thread/src/app/actions/messages.ts
+++ thread/src/app/actions/messages.ts
@@ -1,10 +1,12 @@
 "use server";
 
+import { createServerEnv } from "@/config/env";
 import { revalidatePath } from "next/cache";
 
 export async function getMessages() {
   try {
-    const res = await fetch(`${process.env.API_URL}/messages`);
+    const env = createServerEnv();
+    const res = await fetch(`${env.API_URL}/messages`);
     if (!res.ok)
       throw new Error("Erreur lors de la récupération des messages.");
     return res.json();
@@ -14,10 +16,11 @@ export async function getMessages() {
 }
 
 export async function sendMessage(formData: FormData) {
+  const env = createServerEnv();
   const pseudonym = formData.get("pseudonym");
   const content = formData.get("content");
   try {
-    const res = await fetch(`${process.env.API_URL}/messages`, {
+    const res = await fetch(`${env.API_URL}/messages`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ pseudonym, content }),
